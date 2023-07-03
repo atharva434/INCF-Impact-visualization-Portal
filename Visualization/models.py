e19d1f204d7ac12d79eeb9eba1f95def7e649f7c
atharva434 <atharvaspurohi@gmail.com>
2023-07-03 21:57:26 +0530

    URL loader trial

--- Visualization/models.py
+++ Visualization/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Organization(models.Model):
+    name=models.CharField(max_length=100)
+    domain=models.CharField(max_length=50,blank=True, null=True)
+
+
+
+class Project(models.Model):
+    title=models.CharField(max_length=50)
+    description=models.TextField(blank=True, null=True)
+    organization=models.ForeignKey(Organization,on_delete=models.SET_NULL,null=True)
+    tech_stack=models.CharField(max_length=100) #change max_length = 50
+    domain=models.CharField(max_length=50,blank=True, null=True)
+    subdomain=models.CharField(max_length=50,blank=True, null=True)
+    # phone=models.IntegerField(max_length=10)
+    impact=models.TextField(blank=True, null=True)
+
+
