f190af3398826c3a8eb6becf87d57c813af52c3c
surajgajul <95496170+surajgajul@users.noreply.github.com>
2024-03-14 19:58:12 +0530

    headernfooter

--- Visualization/urls.py
+++ Visualization/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("total",views.total_impact),
     path("org",views.organization),
     path("detail/<int:id>",views.detail),
-    path("chatbot",views.chatbot),
+    path("chatbot",views.chatbot, name="chatbot"),
     path('get_qa_model', views.get_qa_model, name='get_qa_model'),
     path("upload",views.upload),
     path("add/<int:id>",views.add),
