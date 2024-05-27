347493c9a08f2fbc1aa58178a08129bb06021930
surajgajul <95496170+surajgajul@users.noreply.github.com>
2024-05-27 23:31:46 +0530

    upload page

--- Visualization/views.py
+++ Visualization/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .url_retriever import impact,calculator,clean_with_llm
 from .retrievalqna import chat,ingest,ingest_documents,fill_db
 # Create your views here.
+from django.contrib import messages
 from django import template
 from .models import Organization,Project
 from django.core.files.storage import FileSystemStorage
@@ -13,12 +14,12 @@ register = template.Library()
 @register.filter
 def modulo(num, val):
     return num % val == 0
-def home(request):
+
+def adminpage(request):
     organization=Organization.objects.all()
     # urls=list(organization.values_list("impact", flat = True))
     # ingest(urls)
-    
-    return render(request,"home.html")
+    return render(request,"adminpage.html")
 
 def homepage(request):
     organizations=Organization.objects.all()
@@ -27,7 +28,7 @@ def homepage(request):
     urls=list(organizations.values_list("impact", flat = True))
     impact=calculator(urls)
     print(type(impact))
-    return render(request,"newpage.html",{"organization":organizations,"impact":impact})
+    return render(request,"homepage.html",{"organization":organizations,"impact":impact})
 
 def organization(request):
     print("ji")
@@ -41,7 +42,7 @@ def organization(request):
         organization=Organization(name=org_name,url=url,aim=impact_params["aim"],use=impact_params["use"],impact=impact_params["impact"])
         organization.save()
     ingest(url)
-    return render(request,"home.html",{"name":org_name})
+    return render(request,"adminpage.html",{"name":org_name})
 
 
 
@@ -95,8 +96,6 @@ def add(request,id):
         my_instance.organization=org
         my_instance.save()
         return redirect("homepage")
-
-
     return render(request,"projectform.html",{"id":id})
 
 
@@ -110,8 +109,9 @@ def get_qa_model(request):
     # loader = PyPDFLoader(testpdf)
     # pages = loader.load_and_split()
     ingest_documents(testpdf)
-    # print(qa_model)
-    return render(request,"documentChatbot.html",{"document":True})
+    messages.success(request, 'File uploaded successfully.')
+    print("Success")
+    return redirect("homepage")
 
 def upload(request):
     return render(request,"upload_pdf.html")
